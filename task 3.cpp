f76fe3b803ad6a9731188c1b4b99c37ec9d59d12
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2023-03-29 20:57:53 +0700

    Create task 3.cpp

---
+++ task 3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 10; 
+    int* arr = new int[size]; 
+    int index = 0; 
+    int sum = 0; 
+
+    int input; 
+    cin >> input;
+
+    while (input != -1) 
+    {
+        arr[index] = input;
+        sum += input;
+        index++;   
+      
+        cin >> input; // считываем следующее число
+    }
+  
+    cout << sum << endl; // выводим сумму
+
+    return 0;
+}
